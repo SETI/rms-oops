18a67771f2a7d84315f2e950e90c7d1f698a92ca
Barton Wells <bwells@seti.org>
2011-12-02 22:22:59 +0000

    removing python so we can redo directory structure

--- python/CSPICE/make1.sh
+++
@@ -1,9 +0,0 @@
-swig -python cspice.i
-
-gcc -c cspice_wrap.c \
--I/Library/Frameworks/EPD64.framework/Versions/Current/include/python2.7/ \
--I/Library/Frameworks/EPD64.framework/Versions/Current/lib/python2.7/site-packages/numpy/core/include/
-
-ld -bundle -flat_namespace -undefined suppress -o _cspice.so cspice_wrap.o /usr/lib/crt1.o /Tools/lib/libcspice.a -lm
-
-
