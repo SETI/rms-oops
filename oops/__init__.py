6763f3da4670f60ced8b62bc79c077e2332b94e6
Mark Showalter <mshowalter@seti.org>
2012-03-09 05:25:45 +0000

    Updated the parallel oops/ directory

--- oops/__init__.py
+++ oops/__init__.py
@@ -1,6 +1,44 @@
 ################################################################################
 # oops/__init__.py
+#
+# 3/8/12 Created by MRS.
 ################################################################################
+
+# Examples of import statements and how they work:
+#
+# >>> import oops
+#   This imports the entire oops object tree (but not instruments), creating
+#   classes such as oops.Scalar, ops.Event, oops.path.SpicePath, plus other
+#   components such as oops.registry and oops.config. This is the recommended
+#   form of import.
+#
+# >>> import oops as abc
+#   Imports the ENTIRE oops object tree but with the prefix "abc" replacing
+#   "oops".
+#
+# >>> from oops import *
+#   This imports all the oops components without the "oops" prefix. It can fill
+#   up the default name space but there is nothing wrong with it.
+#
+# >>> import oops.array
+#   This statement imports the entire object tree, not just the requested
+#   component. This may be counterintuitive, although importing only selected
+#   components of oops would seem to be of little value.
+#
+# >>> import oops.array as arr
+#   This statement imports Array and its subclasses with the given alias, e.g.,
+#   arr.Array, arr.Scalar, etc. It does NOT import the rest of oops. This can
+#   be a handy way to assign an alias to components of oops, but one should
+#   also import the remaining components in a separate import statement.
+#
+# >>> from oops.array import *
+#   This statement imports the selected component of oops with the "oops"
+#   prefix, e.g., Array, Scalar, Empty. Does not import the remainder of oops.
+#
+# >>> from oops import surface, path
+#   This statement imports the selected components of oops without the "oops"
+#   prefix, e.g., surface.Spheroid and path.SpicePath. It does not import the
+#   remainder of oops.
 
 from oops_.array.all import *
 
