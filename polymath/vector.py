New backplane "ring_gradient_angle" give the angle of the radius gradient in an image, measured from U toward V. Also a few bug fixes. Unit tests pass.

polymath/vector.py
@@ -499,7 +499,7 @@ class Vector(Qube):
         b = b * sign
 
         arg = 0.5 * (a - b).norm()
-        angle = 2. * sign * arg.arcsin(check=False) + (sign < 0.) * np.pi
+        angle = 2. * sign * arg.arcsin() + (sign < 0.) * np.pi
 
         return angle
 