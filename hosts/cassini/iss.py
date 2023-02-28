9f45a90aac5d12897ef571f0a4f64107b390f4a5
Joseph Spitale <jnspitale@gmail.com>
2023-02-28 15:57:27 -0700

    - added gold master tests for cassini iss

--- hosts/cassini/iss.py
+++ hosts/cassini/iss.py
@@ -418,15 +418,16 @@ class ISS(object):
 # UNIT TESTS
 ################################################################################
 
-import unittest
 import os.path
+import unittest
+import oops.backplane.gold_master as gm
 
 from oops.unittester_support            import TESTDATA_PARENT_DIRECTORY
 from oops.backplane.exercise_backplanes import exercise_backplanes
 from oops.backplane.unittester_support  import Backplane_Settings
 
 
-#*******************************************************************************
+#===============================================================================
 class Test_Cassini_ISS(unittest.TestCase):
 
     def runTest(self):
@@ -443,26 +444,80 @@ class Test_Cassini_ISS(unittest.TestCase):
         self.assertTrue(abs(snapshot.time[1] - snapshot3940.time[1]) < 1.e-3)
 
 
-#*******************************************************************************
-class Test_Cassini_ISS_Backplane_Exercises(unittest.TestCase):
+#===============================================================================
+class Test_Cassini_ISS_GoldMaster_N1460072401(unittest.TestCase):
 
     #===========================================================================
     def runTest(self):
+        """
+        N1460072401 Compare w Gold Masters
 
-        if Backplane_Settings.NO_EXERCISES:
-            self.skipTest('')
+        To preview and regenerate gold masters (from pds-oops/oops/backplane/):
+            python gold_master.py \
+                ~/Dropbox-SETI/OOPS-Resources/test_data/cassini/ISS/N1460072401_1.IMG \
+                --module hosts.cassini.iss \
+                --planet SATURN \
+                --ring SATURN_MAIN_RINGS \
+                --preview
 
-        root = os.path.join(TESTDATA_PARENT_DIRECTORY, 'cassini/ISS')
-        file = os.path.join(root, 'N1460072401_1.IMG')
-        obs = from_file(file)
-        exercise_backplanes(obs, use_inventory=True, inventory_border=4,
-                                 planet_key='SATURN')
+            python gold_master.py \
+                ~/Dropbox-SETI/OOPS-Resources/test_data/cassini/ISS/N1460072401_1.IMG \
+                --module hosts.cassini.iss \
+                --planet SATURN \
+                --ring SATURN_MAIN_RINGS \
+                --adopt
+        """
+        gm.execute_as_unittest(self,
+                obspath = os.path.join(TESTDATA_PARENT_DIRECTORY,
+                                       'cassini/ISS/N1460072401_1.IMG'),
+                index   = None,
+                module  = 'hosts.cassini.iss',
+                planet  = 'SATURN',
+                moon    = '',
+                ring    = 'SATURN_MAIN_RINGS',
+                inventory=False, border=10)
+
+
+#===============================================================================
+class Test_Cassini_ISS_GoldMaster_W1573721822_1(unittest.TestCase):
+
+    #===========================================================================
+    def runTest(self):
+        """
+        W1573721822 Compare w Gold Masters
+
+        To preview and regenerate gold masters (from pds-oops/oops/backplane/):
+            python gold_master.py \
+                ~/Dropbox-SETI/OOPS-Resources/test_data/cassini/ISS/W1573721822_1.IMG \
+                --module hosts.cassini.iss \
+                --planet SATURN \
+                --moon EPIMETHEUS \
+                --ring SATURN_MAIN_RINGS \
+                --preview
+
+            python gold_master.py \
+                ~/Dropbox-SETI/OOPS-Resources/test_data/cassini/ISS/W1573721822_1.IMG \
+                --module hosts.cassini.iss \
+                --planet SATURN \
+                --moon EPIMETHEUS \
+                --ring SATURN_MAIN_RINGS \
+                --adopt
+        """
+        gm.execute_as_unittest(self,
+                obspath = os.path.join(TESTDATA_PARENT_DIRECTORY,
+                                       'cassini/ISS/W1573721822_1.IMG'),
+                index   = None,
+                module  = 'hosts.cassini.iss',
+                planet  = 'SATURN',
+                moon    = 'EPIMETHEUS',
+                ring    = 'SATURN_MAIN_RINGS',
+                inventory=False, border=10)
 
 
 ############################################
 from oops.backplane.unittester_support import backplane_unittester_args
 
-if __name__ == '__main__': # pragma: no cover
+if __name__ == '__main__':
     backplane_unittester_args()
     unittest.main(verbosity=2)
 ################################################################################
