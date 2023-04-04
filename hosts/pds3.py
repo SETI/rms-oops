623adc7c221a3d853e7c7b22f040efb72515a27b
Joseph Spitale <jnspitale@gmail.com>
2023-04-04 14:22:49 -0700

    - switched to new pds3 code usage

--- hosts/pds3.py
+++ hosts/pds3.py
@@ -8,20 +8,20 @@ import os
 import pdsparser
 from pathlib import Path
 
-
-#===========================================================================
-def clean_comments(line):
+#===============================================================================
+def clean_comments(lines):
     """
     Standard cleaner to handle commenting errors that could affect any host.
     """
-
     # Close-comment w/o open-comment
-    if line.strip().endswith('*/') and '/*' not in line:
-        line = '\n'
+    for i in range(len(lines)):
+        line = lines[i]
+        if line.strip().endswith('*/') and '/*' not in line:
+            lines[i] = '\n'
 
-    return line
+    return lines
 
-#===========================================================================
+#===============================================================================
 def call_cleaners(cleaners, lines):
     """
     Call the given cleaner functions.
@@ -30,41 +30,36 @@ def call_cleaners(cleaners, lines):
         cleaners:       List of cleaner functions.
         lines:          List of lines comprising the label.
     """
-
-    for i in range(len(lines)):
-        for cleaner in cleaners:
-            lines[i] = cleaner(lines[i])
+    for cleaner in cleaners:
+        lines = cleaner(lines)
 
     return lines
 
-#===========================================================================
-def clean_label(lines, cleaners=None, no_standard=False):
+#===============================================================================
+def clean_label(lines, cleaner=None, no_standard=False):
     """
     Correct known errors in PDS3 labels.
 
     Inputs:
         lines:          List of lines comprising the label.
-        cleaners:       List of functions that take a line as input and
-                        return a cleaned line.  By default, these functions
-                        are appended to a list of standard cleaner functions.
+        cleaner:        Function that takes a line as input and
+                        return a cleaned line.
         no_standard:    If True, the standard cleaners are not used.
     """
-
     # Call standard cleaners
     standard_cleaners = [clean_comments]
     if not no_standard:
         lines = call_cleaners(standard_cleaners, lines)
 
-    # Call custom cleaners
-    if cleaners is not None:
-        lines = call_cleaners(cleaners, lines)
+    # Call custom cleaner
+    if cleaner is not None:
+        lines = call_cleaners([cleaner], lines)
 
     return lines
 
-#===========================================================================
+#===============================================================================
 def find_label(filespec):
     """Find the PDS3 label corresponding to the given filespec."""
-
     # Construct candidate label filenames
     spec = Path(filespec)
     labelspecs = [
@@ -81,19 +76,17 @@ def find_label(filespec):
     # If no successful candidate, assume attached label
     return filespec
 
-#===========================================================================
-def get_label(filespec, cleaners=None, no_standard=False):
+#===============================================================================
+def get_label(filespec, cleaner=None, no_standard=False):
     """
     Find the PDS3 label, clean it, load it, and parse into dictionary.
 
     Inputs:
         filespec:       File specification for the label.
-        cleaners:       List of functions that take a label line as input and
-                        return a cleaned line.  By default, these functions
-                        are appended to a list of standard cleaner functions.
+        cleaner:        Function that takes a line as input and
+                        return a cleaned line.
         no_standard:    If True, the standard cleaners are not used.
     """
-
     assert os.path.isfile(filespec), f'Not found: {filespec}'
 
     # Find the label
@@ -101,7 +94,7 @@ def get_label(filespec, cleaners=None, no_standard=False):
 
     # Load and clean the PDS label
     lines = clean_label(pdsparser.PdsLabel.load_file(labelspec),
-                        cleaners=cleaners, no_standard=no_standard)
+                        cleaner=cleaner, no_standard=no_standard)
 
     # Parse the label into a dictionary
     return pdsparser.PdsLabel.from_string(lines).as_dict()
@@ -116,22 +109,25 @@ import os.path
 from hosts                   import pds3
 from oops.unittester_support import TESTDATA_PARENT_DIRECTORY
 
+#===========================================================================
+def clean_VIMS(lines):
+    for i in range(len(lines)):
+        lines[i] = lines[i].replace('(N/A',  '("N/A"')
+        lines[i] = lines[i].replace('N/A)',  '"N/A")')
+
+    return lines
 
 #===========================================================================
-def clean_VIMS(line):
-    line = line.replace('(N/A',  '("N/A"')
-    line = line.replace('N/A)',  '"N/A")')
-    return line
+def clean_UVIS(lines):
+    for i in range(len(lines)):
+        line = lines[i]
+        if 'CORE_UNIT' in line:
+            if '"COUNTS/BIN"' not in line:
+                liness[i] = line.replace('COUNTS/BIN', '"COUNTS/BIN"')
+        if line.startswith('ODC_ID'):
+            lines[i] = lines[i].replace(',', '')
 
-#===========================================================================
-def clean_UVIS(line):
-    if 'CORE_UNIT' in line:
-        if '"COUNTS/BIN"' not in line:
-            lines = line.replace('COUNTS/BIN', '"COUNTS/BIN"')
-    if line.startswith('ODC_ID'):
-        line = line.replace(',', '')
-    return line
-
+    return lines
 
 #===============================================================================
 class Test_PDS3(unittest.TestCase):
@@ -156,13 +152,13 @@ class Test_PDS3(unittest.TestCase):
         # Test Cassini VIMS file
         filespec = 'cassini/VIMS/v1793917030_1.lbl'
         label_dict = pds3.get_label(os.path.join(TESTDATA_PARENT_DIRECTORY, filespec),
-                                    cleaners=[clean_VIMS])
+                                    cleaner=clean_VIMS)
         self.assertTrue(label_dict['PDS_VERSION_ID'] == 'PDS3')
 
         # Test Cassini UVIS file
         filespec = 'cassini/UVIS/HSP2014_197_21_29.LBL'
         label_dict = pds3.get_label(os.path.join(TESTDATA_PARENT_DIRECTORY, filespec),
-                                                 cleaners=[clean_UVIS])
+                                                 cleaner=clean_UVIS)
         self.assertTrue(label_dict['PDS_VERSION_ID'] == 'PDS3')
 
 ##############################################
