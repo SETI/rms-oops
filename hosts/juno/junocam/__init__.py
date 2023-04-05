57301923350595c5e8a4fd16dfc062cc6f704799
Joseph Spitale <jnspitale@gmail.com>
2023-04-05 12:18:20 -0700

    - use fully qualified path for standard_obs import

--- hosts/juno/junocam/__init__.py
+++ hosts/juno/junocam/__init__.py
@@ -455,57 +455,12 @@ class Test_Juno_Junocam_GoldMaster(unittest.TestCase):
 #    #===========================================================================
 #    def test_JNCR_2019096_19M00012_V02(self):
 #        gm.execute_as_unittest(self, 'JNCR_2019096_19M00012_V02')
-#
-#    #===========================================================================
-#    def test_JNCR_2019149_20G00008_V01(self):
-#        gm.execute_as_unittest(self, 'JNCR_2019149_20G00008_V01')
+
+    #===========================================================================
+    def test_JNCR_2019149_20G00008_V01(self):
+        gm.execute_as_unittest(self, 'JNCR_2019149_20G00008_V01')
 
 
-#    #===========================================================================
-#    def runTest(self):
-#        """
-#        Gold master tests for Juno Junocam.
-#
-#        Docstrings used below to facilitate copy/pasting of multi-line commands.
-#        """
-#
-#        ############################
-#        # JNCR_2016347_03C00192_V01
-#        ############################
-#        """
-#        To preview and regenerate gold masters (from pds-oops/oops/backplane/):
-#
-#        python gold_master.py \
-#            ~/Dropbox-SETI/OOPS-Resources/test_data/juno/junocam/03/JNCR_2016347_03C00192_V01.img \
-#            --module hosts.juno.junocam \
-#            --planet JUPITER \
-#            --no-inventory \
-#            --preview
-#
-#        python gold_master.py \
-#            ~/Dropbox-SETI/OOPS-Resources/test_data/cassini/ISS/JNCR_2016347_03C00192_V01.img \
-#            ~/Dropbox-SETI/OOPS-Resources/test_data/juno/junocam/03/JNCR_2016347_03C00192_V01.img \
-#            --module hosts.juno.junocam \
-#            --planet JUPITER \
-#            --no-inventory \
-#            --adopt
-#        """
-#        gm.override('Celestial north minus east angles (deg)', 8.)
-#        gm.override('JUPITER:RING azimuth minus longitude wrt Sun (deg)', None)
-#        gm.override('JUPITER:RING emission angle, ring minus center (deg)', None)
-#        gm.override('JUPITER:RING incidence angle, ring minus center (deg)', 3.)
-#
-#        gm.execute_as_unittest(self,
-#                obspath = os.path.join(TESTDATA_PARENT_DIRECTORY,
-#                                       'juno/junocam/03/'
-#                                       'JNCR_2016347_03C00192_V01.img'),
-#                index   = 5,
-#                module  = 'hosts.juno.junocam',
-#                planet  = 'JUPITER',
-#                moon    = '',
-#                ring    = '',
-#                kwargs  = {'snap': False},
-#                inventory=False, border=10)     # overrides of defaults
 
 ##############################################
 if __name__ == '__main__':
