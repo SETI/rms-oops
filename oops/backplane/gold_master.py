d0cd565e7badb4afd6beeac5ff803ce7565ecf65
Joseph Spitale <jnspitale@gmail.com>
2023-04-19 08:47:45 -0700

    - removed errors attribute from BackplaneTest class

--- oops/backplane/gold_master.py
+++ oops/backplane/gold_master.py
@@ -1048,7 +1048,6 @@ class BackplaneTest(object):
         self.gold_summary_ = None
         self.summary = {}
         self.results = {}
-        self.errors = 0
 
     ############################################################################
     # Test runner for one BackplaneTest
