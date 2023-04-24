a2a4c71adbd049245b10ab748d4464c2e4c24155
Joseph Spitale <jnspitale@gmail.com>
2023-04-24 10:55:18 -0700

    cleanup

--- hosts/galileo/ssi/__init__.py
+++ hosts/galileo/ssi/__init__.py
@@ -308,23 +308,23 @@ import oops.backplane.gold_master as gm
 
 from oops.unittester_support    import TESTDATA_PARENT_DIRECTORY
 
-
 #===============================================================================
 class Test_Galileo_SSI_GoldMaster(unittest.TestCase):
 
     #===========================================================================
-    def test_C0349632100R(self):
+    def setUp(self):
         from hosts.galileo.ssi import standard_obs
+
+    #===========================================================================
+    def test_C0349632100R(self):
         gm.execute_as_unittest(self, 'C0349632100R')
 
     #===========================================================================
     def test_C0368369200R(self):
-        from hosts.galileo.ssi import standard_obs
         gm.execute_as_unittest(self, 'C0368369200R')
 
     #===========================================================================
     def test_C0061455700R(self):
-        from hosts.galileo.ssi import standard_obs
         gm.execute_as_unittest(self, 'C0061455700R')
 
 ############################################
--- hosts/juno/junocam/__init__.py
+++ hosts/juno/junocam/__init__.py
@@ -443,28 +443,27 @@ import oops.backplane.gold_master as gm
 
 from oops.unittester_support import TESTDATA_PARENT_DIRECTORY
 
-
 #===============================================================================
 class Test_Juno_Junocam_GoldMaster(unittest.TestCase):
 
     #===========================================================================
-    def test_JNCR_2016347_03C00192_V01(self):
+    def setUp(self):
         from hosts.juno.junocam import standard_obs
+
+    #===========================================================================
+    def test_JNCR_2016347_03C00192_V01(self):
         gm.execute_as_unittest(self, 'JNCR_2016347_03C00192_V01')
 
     #===========================================================================
     def test_JNCR_2020366_31C00065_V01(self):
-        from hosts.juno.junocam import standard_obs
         gm.execute_as_unittest(self, 'JNCR_2020366_31C00065_V01')
 
     #===========================================================================
     def test_JNCR_2019096_19M00012_V02(self):
-        from hosts.juno.junocam import standard_obs
         gm.execute_as_unittest(self, 'JNCR_2019096_19M00012_V02')
 
     #===========================================================================
     def test_JNCR_2019149_20G00008_V01(self):
-        from hosts.juno.junocam import standard_obs
         gm.execute_as_unittest(self, 'JNCR_2019149_20G00008_V01')
 
 
