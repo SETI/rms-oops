- corrected percentile calculation in lighting.py - added overrides to standard_obs.py

oops/backplane/lighting.py
@@ -409,10 +409,10 @@ def lighting_test_suite(bpt):
 
         # Select the 95th percentile as a large but representative value
         # This is needed because km_per_fov_radian diverges near limb
-        from IPython import embed; print('+++++++++++++'); embed()
-        values = deg_per_fov_radian.vals[deg_per_fov_radian.antimask].flatten()
+        values = deg_per_fov_radian[deg_per_fov_radian.antimask].vals
         if values.size == 0:
             continue
+        values = np.sort(values.flatten())
         cutoff = (values.size * 95) // 100
         deg_per_fov_radian = values[cutoff]
 