092b5cb09fbd6f6e7f6f0ddd84880a8753962b82
Mark Showalter <mshowalter@seti.org>
2023-09-20 17:13:41 -0700

    _Should_ fix the metadata bug

--- oops/surface/centricellipsoid.py
+++ oops/surface/centricellipsoid.py
@@ -22,14 +22,18 @@ class CentricEllipsoid(Ellipsoid):
             pos         a Vector3 of positions at or near the surface, relative
                         to this surface's origin and frame.
             obs         a Vector3 of observer position relative to this
-                        surface's origin and frame; ignored here.
-            time        a Scalar time at which to evaluate the surface; ignored.
-            axes        2 or 3, indicating whether to return a tuple of two or
-                        three Scalar objects.
+                        surface's origin and frame; ignored for this Surface
+                        subclass.
+            time        a Scalar time at which to evaluate the surface; ignored
+                        for this Surface subclass.
+            axes        2 or 3, indicating whether to return the first two
+                        coordinates (lon, lat) or all three (lon, lat, z) as
+                        Scalars.
             derivs      True to propagate any derivatives inside pos and obs
                         into the returned coordinates.
-            hints       if provided, the value of the coefficient p such that
-                            ground + p * normal(ground) = pos
+            hints       optionally, the value of the coefficient p such that
+                            ground + p * normal(ground) = pos;
+                        ignored if the value is None (the default) or True.
             groundtrack True to return the intercept on the surface along with
                         the coordinates.
 
@@ -38,7 +42,7 @@ class CentricEllipsoid(Ellipsoid):
             lat         latitude at the surface in radians.
             z           vertical altitude in km normal to the surface; included
                         if axes == 3.
-            groundtrack intecept point on the surface (where z == 0); included
+            track       intercept point on the surface (where z == 0); included
                         if input groundtrack is True.
         """
 
@@ -48,12 +52,12 @@ class CentricEllipsoid(Ellipsoid):
         pos = Vector3.as_vector3(pos, recursive=derivs)
 
         # Use the quick solution for the body points if hints are provided
-        if hints is not None:
+        if isinstance(hints, (type(None), bool, np.bool_)):
+            (track, p) = self.intercept_normal_to(pos, guess=True)
+        else:
             p = Scalar.as_scalar(hints, recursive=derivs)
             denom = Vector3.ONES + p * self.unsquash_sq
             track = pos.element_div(denom)
-        else:
-            (track, p) = self.intercept_normal_to(pos, guess=True)
 
         # Derive the coordinates
         (x,y,z) = track.to_scalars()
@@ -79,26 +83,26 @@ class CentricEllipsoid(Ellipsoid):
 
         Input:
             coords      a tuple of two or three Scalars defining coordinates at
-                        or near this surface.
+                        or near this surface. These can have different shapes,
+                        but must be broadcastable to a common shape.
                 lon     longitude at the surface in radians.
                 lat     latitude at the surface in radians.
                 z       vertical altitude in km normal to the body surface.
             obs         a Vector3 of observer position relative to this
-                        surface's origin and frame; ignored here.
-            time        a Scalar time at which to evaluate the surface; ignored.
+                        surface's origin and frame; ignored for this Surface
+                        subclass.
+            time        a Scalar time at which to evaluate the surface; ignored
+                        for this Surface subclass.
             derivs      True to propagate any derivatives inside the coordinates
                         and obs into the returned position vectors.
             groundtrack True to include the associated groundtrack points on the
                         body surface in the returned result.
 
-        Return:         pos or (pos, groundtrack), where
+        Return:         pos or (pos, track), where
             pos         a Vector3 of points defined by the coordinates, relative
                         to this surface's origin and frame.
-            groundtrack True to include the associated groundtrack points on the
-                        body surface in the returned result.
-
-        Note that the coordinates can all have different shapes, but they must
-        be broadcastable to a single shape.
+            track       intercept point on the surface (where z == 0); included
+                        if input groundtrack is True.
         """
 
         # Validate inputs
